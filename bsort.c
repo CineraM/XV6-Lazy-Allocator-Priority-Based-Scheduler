ba1d630409a9678ce936ec84cbd1ac997f2b66d0
Matias <64340009+CineraM@users.noreply.github.com>
2022-11-15 01:05:26 -0500

    Update bsort.c

--- bsort.c
+++ bsort.c
@@ -4,11 +4,13 @@
 #include "fcntl.h"
 
 #define STRING_SIZE 100
-#define N 500
+#define N 400
 // store strings, eval strings
 char file_contents[N][STRING_SIZE];
 char eval[N][STRING_SIZE];
 int eval_bool[N];
+
+
 
 // buffer variables
 char buff;
@@ -69,9 +71,14 @@ void truncate_strings(int num_of_lines)
         // finding alpha, or numeric character index in current string
         for(z=0; z<STRING_SIZE; z++)
         {
+            // if(file_contents[i][z] != ' ')
+            //     continue;
+            // else
+            //     break;
             if (file_contents[i][z] >= 48 && file_contents[i][z] <= 57) break;  // digits
             if (file_contents[i][z] >= 65 && file_contents[i][z] <= 90) break;  // cap
             if (file_contents[i][z] >= 97 && file_contents[i][z] <= 122) break; // lower
+            if (file_contents[i][z] == '-') break;  // possible negative numbers
             if (file_contents[i][z] == '\0') break;
         }
         if(z==STRING_SIZE || file_contents[i][z] == '\0')    // if there is not a numeric || alpha char, copy all the string
@@ -90,6 +97,9 @@ void truncate_strings(int num_of_lines)
         // copy the string into eval
         for(z=0; z<STRING_SIZE;z++) 
         {
+            // 0 == caps or other characters
+            // 1 == lowecase
+            // 2 ==  numbers
             if(z==0)
             {
                 if(temp_str[0] >= 'A' && temp_str[0] <= 'Z')
@@ -99,7 +109,16 @@ void truncate_strings(int num_of_lines)
                 else if(temp_str[0] >= 'a' && temp_str[0] <= 'z')  
                 {
                     eval_bool[i] = 1;
-                }  
+                }
+                else if(temp_str[0] >= '0' && temp_str[0] <= '9')
+                {
+                    eval_bool[i] = 2;
+                }
+                else if(temp_str[0] == '-')
+                {
+                    if(temp_str[1] >= '0' && temp_str[1] <= '9')
+                        eval_bool[i] = 2;
+                }
             }
             eval[eidx][z] = temp_str[z];
         }
@@ -107,7 +126,7 @@ void truncate_strings(int num_of_lines)
     }
 }
 
-void bubble_sort(int n)
+void sort(int n)
 {
     int i, j, k, flag;
     for(i=0; i<=n; i++)
@@ -118,7 +137,7 @@ void bubble_sort(int n)
             int compare = strcmp(eval[i], eval[j]);
             if(compare == 0)
             {
-                if(eval_bool[j] && !eval_bool[i])
+                if(eval_bool[j] == 1 && eval_bool[i] != 1)
                     flag=1;
             }
             if(compare > 0 || flag)
@@ -140,6 +159,103 @@ void bubble_sort(int n)
     }
 }
 
+
+char nums[N][STRING_SIZE]; 
+int eval_n[N];
+
+void sort_n(int n)
+{
+    truncate_strings(n);
+    char str_num[STRING_SIZE];
+    int i, j, k, isNum, isNeg;
+    int nums_index = 0;
+
+    for(i=0; eval[i][0]!='\0'; i++)
+    {
+        isNum=0;
+        isNeg=0;
+        if (eval[i][0] >= 48 && eval[i][0] <= 57) isNum=1;  // digits
+        if (eval[i][0] == '-')
+        {
+            if (eval[i][1] >= 48 && eval[i][1] <= 57)
+                isNeg=1;
+        }
+
+        // copy nums into
+        if(isNeg || isNum)
+        {
+            j=0;
+            for(k=0; k<STRING_SIZE; k++)
+            {
+                if(k==0) if(isNeg) continue;
+
+                if(eval[i][1] < 48 && eval[i][1] > 57) break;
+                str_num[j] = eval[i][k];
+                j++;
+            }
+            str_num[j] = '\0';
+            for(k=0; k<STRING_SIZE; k++) nums[nums_index][k] = file_contents[i][k];
+            
+            eval_n[nums_index] = atoi(str_num);
+            if(isNeg)
+                eval_n[nums_index] *= -1;
+
+            nums_index++;
+            memset(str_num, 0, STRING_SIZE);
+        }
+    }
+
+    int temp_n;
+    // bouble sort -n
+    for(i=0; i<=nums_index; i++)
+    {                 
+        for(j=i+1; j<=nums_index; j++)
+        {
+            if(eval_n[i]>eval_n[j])
+            {
+                // original elements
+                for(k=0; k<STRING_SIZE;k++) temp_str[k] = nums[i][k];
+                for(k=0; k<STRING_SIZE;k++) nums[i][k] = nums[j][k]; 
+                for(k=0; k<STRING_SIZE;k++) nums[j][k] = temp_str[k];
+
+                // numerical values
+                temp_n = eval_n[i];
+                eval_n[i] = eval_n[j];
+                eval_n[j] = temp_n;
+            }
+        }
+    }
+
+    sort(n);
+    
+    // print -n
+    int flag=1;
+    for(i=0; i<=nums_index; i++)
+    {
+        if(0<eval_n[i] && flag) // will only print once
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if(eval_bool[j] == 2) continue;
+                printf(1, "%s", file_contents[j]);
+            } 
+            flag=0;
+        }
+        printf(1, "%s", nums[i]);
+    }
+
+    if(nums_index == 0 || flag)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if(eval_bool[j] == 2) continue;
+            printf(1, "%s", file_contents[j]);
+        }  
+    }
+
+}
+
+
 int main(int argc, char *argv[])
 {
     int fd = 0;
@@ -152,8 +268,7 @@ int main(int argc, char *argv[])
         }
         int num_of_lines = read_contents(fd);
         truncate_strings(num_of_lines);
-        bubble_sort(num_of_lines);
-
+        sort(num_of_lines);
 
         // print contents
         for (int i = 0; i <= num_of_lines; i++)
@@ -162,8 +277,24 @@ int main(int argc, char *argv[])
             printf(1, "%s", file_contents[i]);
         } 
 
-        close(fd);
+    }
+    else if(argc == 3)
+    {
+        if((fd = open(argv[1], 0)) < 0)
+        {
+            printf(1, "sort: cannot open file %s\n", argv[1]);
+            exit();
+        }
+
+        int num_of_lines = read_contents(fd);
+        sort_n(num_of_lines);
+    }
+    else
+    {
+        printf(1, "Error: Wrong args\n");
+        exit();
     }
 
+    close(fd);
     exit();
 }
